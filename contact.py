1b1885c4f21e0508b8505ae3b7c6e2909ace4088
pavan <pavankumarreddy155@gmail.com>
2025-03-17 11:56:48 +0530

    [Pavan] Add: created contact using python script.

---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contact:
+    def __init__(self,first_name,last_name,address,state,city,zip_code,phone_number,mail):
+        self.first_name=first_name
+        self.last_name=last_name
+        self.address=address
+        self.state=state
+        self.city=city
+        self.zip_code=zip_code
+        self.phone_number=phone_number
+        self.mail=mail
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} | {self.phone_number} | {self.email_id} | {self.city} | {self.state} | {self.zip_code}"
